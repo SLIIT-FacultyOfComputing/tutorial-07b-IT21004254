14078243bdb0d5b4207ca7d41beba151108a113c
IT21004254 <87602692+IT21004254@users.noreply.github.com>
2021-08-29 11:15:23 +0000

    IT21004254 is done with tutorial 7

--- Item.cpp
+++ Item.cpp
@@ -6,6 +6,19 @@ using namespace std;
 // 5. Implement Overloaded Constructor Implementation
 // 6. Implement Destructor (display "Destructor Called")
 
+Item::Item()
+{
+  itemCode = 0;
+  unitPrice = 0.0;
+  discount = 0.0;
+}
+
+Item::Item(int pitemcode, float punitPrice)
+{
+  itemCode = pitemcode;
+  unitPrice = punitPrice;
+}
+
 void Item::setDiscount(float pdiscount) {
   discount = pdiscount;
 }
@@ -21,4 +34,8 @@ float Item::discountedPrice() {
 void Item::display() {
   cout << "Item : " << itemCode << endl;
   cout << "Discounted Price " << discountedPrice() << endl;
+}
+Item::~Item()
+{
+  cout << "Destructor Called" << endl;
 }
